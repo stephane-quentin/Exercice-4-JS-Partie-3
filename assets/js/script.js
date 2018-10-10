9e7dc49a059f36977538df122c3552266bd32a12
Stiouf <stephane.quentin@live.fr>
2018-10-10 09:57:13 +0200

    ex4

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,18 @@
 function verification(){
   var pwd = document.getElementById('password').value;
   var confirm = document.getElementById('confirmPassword').value;
-  if (!isNaN(pwd) || !isNaN(confirm) || pwd < 0 || confirm < 0) {
+  if (!isNaN(pwd) || !isNaN(confirm) || !pwd || !confirm) { //! devant une variable vérifie que la variable n'est pas vide (le champ pour nous)
     alert('Veuillez mettre au moins une lettre');
+  } else if (pwd.length <= 5){ // vérifie que la variable pwd fait au moins 6 caractères de long
+    alert('Le mot de passe est trop court, au moins 6 caractères');
   } else if (pwd == confirm){
     document.getElementById('confirmPassword').style.border = 'green 3px solid';
     document.getElementById('password').style.border = 'green 3px solid';
+    document.getElementById('bravo').innerHTML = 'Les mots de passe sont identiques';
   }
-    else {
-      document.getElementById('confirmPassword').style.border = 'red 3px solid';
-      document.getElementById('password').style.border = 'red 3px solid';
-    }
+  else {
+    document.getElementById('confirmPassword').style.border = 'red 3px solid';
+    document.getElementById('password').style.border = 'red 3px solid';
+    document.getElementById('bravo').innerHTML = 'Les mots de passe sont différents';
+  }
 }
