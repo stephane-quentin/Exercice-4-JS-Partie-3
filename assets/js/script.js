80821356c61a5c8826f695466f86d48f4032ec94
Stiouf <stephane.quentin@live.fr>
2018-10-10 10:13:35 +0200

    ex4

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 function verification(){
   var pwd = document.getElementById('password').value;
   var confirm = document.getElementById('confirmPassword').value;
-  if (!isNaN(pwd) || !isNaN(confirm) || !pwd || !confirm) { //! devant une variable vérifie que la variable n'est pas vide (le champ pour nous)
+  if (!isNaN(pwd) || !isNaN(confirm) || !pwd || !confirm) { //! devant une variable vérifie que la variable n'est pas vide mais pas que (null, undefined, false, 0 et NaN), à utiliser avec précaution
     alert('Veuillez mettre au moins une lettre');
   } else if (pwd.length <= 5){ // vérifie que la variable pwd fait au moins 6 caractères de long
     alert('Le mot de passe est trop court, au moins 6 caractères');
